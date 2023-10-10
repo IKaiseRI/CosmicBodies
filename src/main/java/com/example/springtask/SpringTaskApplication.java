839301c59361cf7c7b097899019482b608513a8f
Ion Vornovitchi <ariathvs@gmail.com>
2023-10-10 10:13:13 +0300

    Added xml spring beans. DB is configured via application context file.

--- src/main/java/com/example/springtask/SpringTaskApplication.java
+++ src/main/java/com/example/springtask/SpringTaskApplication.java
@@ -2,8 +2,10 @@ package com.example.springtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:configuration.xml")
 public class SpringTaskApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springtask/controller/PlanetController.java
+++ src/main/java/com/example/springtask/controller/PlanetController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/planets")
 public class PlanetController {
